a2cead92b6016ade19960604f27b562456ea40bd
Lakshya Purwar <purwarlakshya@gmail.com>
2023-01-14 17:09:21 +0530

    useState in the useInput hook replaced by useReducer

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -1,28 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
+
+//Lets make use of useReducer
+//1.First we make the stateReducer function that 
+//accepts two arguments , previous state snapshot
+//and the dispatched action
+//In this function ,we check the type mentioned in the 
+//action object and accordingly return the new state.
+
+const initialInputState = {
+    value: '',
+    isTouched: false,
+}
+const inputStateReducer = (prevState, action) => {
+    if (action.type == 'VALUE') {
+        return { ...prevState, value: action.newValue };
+
+    }
+    if (action.type == 'BLUR') {
+        return { ...prevState, isTouched: true };
+    }
+    if (action.type == 'RESET') {
+        return { ...prevState, value: '', isTouched: false };
+
+    }
+
+    return initialInputState;
+}
 
 const useInput = (isValid) => {
 
-    const [inputValue, setInputValue] = useState('');
-    const [inputFieldTouched, setInputFieldIsTouched] = useState(false);
-    const  inputValueIsValid = isValid(inputValue);
-    const  isError = !inputValueIsValid && inputFieldTouched;
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+
+
+
+    const inputValueIsValid = isValid(inputState.value);
+    const isError = !inputValueIsValid && inputState.isTouched;
 
     const inputValueChangeHandler = (event) => {
-        setInputValue(event.target.value);
+        // setInputValue(event.target.value);
+        dispatch({ type: 'VALUE', newValue: event.target.value });
     }
 
     const inputFieldBlurHandler = () => {
         //Blur means it lost focus
-        setInputFieldIsTouched(true);
+        // setInputFieldIsTouched(true);
+        dispatch({ type: 'BLUR' });
     }
 
-    const reset = ()=>{
-        setInputValue('');
-        setInputFieldIsTouched(false);
+    const reset = () => {
+        // setInputValue('');
+        // setInputFieldIsTouched(false);
+        dispatch({ type: 'RESET' })
     }
 
     return {
-        inputValue ,
+        inputValue: inputState.value,
         inputValueIsValid,
         isError,
         inputValueChangeHandler,
